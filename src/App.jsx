b75e37bc9a715f246d71bed7757428e8fa07e856
Jyoti Sharma <minikanva@gmail.com>
2026-04-22 17:09:17 -0500

    fixed Services page and added .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
-import Services from "./pages/Services";
+import Services from "./pages/services"
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import Blog from "./pages/Blog";
